c401019d7e032cf4460d476c841fe280f7ea80b1
doubletao318 <futaotao93318@163.com>
2019-10-15 11:26:15 +0800

    Release 2.1.RC3

--- Cinder/Pike/huawei_utils.py
+++ Cinder/Pike/huawei_utils.py
@@ -182,6 +182,9 @@ def get_volume_lun_id(client, volume):
         volume_name = old_encode_name(volume.id)
         lun_id = client.get_lun_id_by_name(volume_name)
 
+    if not lun_id:
+        lun_id = metadata.get("huawei_lun_id")
+
     return lun_id, metadata.get('huawei_lun_wwn')
 
 
